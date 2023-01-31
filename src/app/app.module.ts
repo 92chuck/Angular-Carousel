9232d6158e24d1b0f61359f604561b5a0f79850e
Junbeom Chun <92chuck@gmail.com>
2023-01-31 14:05:53 -0500

    final version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CarouselModule } from './carousel/carousel.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CarouselComponent],
+  imports: [BrowserModule, CarouselModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
